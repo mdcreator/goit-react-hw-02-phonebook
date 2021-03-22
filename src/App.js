9396627dd03d0f283a857e0b56a9898e149b1742
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-03-23 01:56:51 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import Container from "./components/Container";
+import { Component } from 'react';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
 
-export default function App() {
-  return (
-    <div>
-      <Container/>
-    </div>
-  );
+// import Container from './components/Container';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  formSubmitHandler = data => {};
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </div>
+    );
+  }
 }
 
+export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ContactForm extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+  };
+
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleOnSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleOnSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+        </label>
+
+        <label>
+          Number
+          <input
+            type="text"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleInputChange}
+          />
+        </label>
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
